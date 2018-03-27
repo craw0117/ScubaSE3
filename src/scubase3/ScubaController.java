7e56935ab67753cc7810500051c97df0bb414778
simcr <simcr@10.14.2.142>
2018-03-27 10:59:56 +1030

    Added JavaDoc to ScubaController

--- src/scubase3/ScubaController.java
+++ src/scubase3/ScubaController.java
@@ -9,6 +9,11 @@ public class ScubaController {
     private final ScubaModel model;
     private final ScubaFrame view;
 
+    /**
+     * Default constructor, only one instance of this object should exist at any
+     * one time. This application is designed using a loose implementation of
+     * the MVC architecture pattern.
+     */
     public ScubaController() {
         this.view = new ScubaFrame(this);
         this.model = new ScubaModel(view);
@@ -16,35 +21,65 @@ public class ScubaController {
         view.update();
     }
 
+    /**
+     * Provides external access to the model object.
+     *
+     * @return ScubaModel
+     */
     public ScubaModel getModel() {
         return model;
     }
 
+    /**
+     * Provides external access to the view object.
+     *
+     * @return ScubaFrame
+     */
     public ScubaFrame getView() {
         return view;
     }
-    
+
     /**
      * Sets the calculation to perform and will enable the correct inputs.
      *
-     * @param calcType Code for calc ["EAD", "MOD", "MIX", "PP", "SMOD"]
+     * @param calcType Valid calcTypes are "EAD", "MOD", "MIX", "PP", and "SMOD"
      */
     public void setCalcType(String calcType) {
         model.setCalculationType(calcType);
     }
 
+    /**
+     * Sets the partial pressure value and updates the view.
+     *
+     * @param value Accepts values between 1.1 and 1.6
+     */
     public void setPartialPressure(double value) {
         model.setPartialPressure(value);
     }
 
+    /**
+     * Sets the oxygen percentage and updates the view.
+     *
+     * @param value Accepts values between 0 and 1.
+     */
     public void setFractionOxygen(double value) {
         model.setFractionOxygen(value);
     }
 
+    /**
+     * Sets the depth and updates the view.
+     *
+     * @param value
+     */
     public void setDepth(double value) {
         model.setDepth(value);
     }
-    
+
+    /**
+     * Sets the table type to display and updates the view.
+     *
+     * @param type Valid types are "EAD", and "PP"
+     */
     public void setTableType(String type) {
         model.setTableType(type);
     }
