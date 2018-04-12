5562057204b99c091cbff2fe4e4653a24e7429c3
lyeqi <lyeqi@DESKTOP-15KLKB7>
2018-04-13 06:37:41 +0930

    format

--- test/scubase3/ScubaCalculationsTest.java
+++ test/scubase3/ScubaCalculationsTest.java
@@ -75,7 +75,8 @@ public class ScubaCalculationsTest {
         assertEquals(expResult, result);
 
     }
-@Test
+
+    @Test
     public void testCalculateBMLowerBoundary() {
         System.out.println("calculateBM");
         double in1 = 1.3;
@@ -85,7 +86,8 @@ public class ScubaCalculationsTest {
         assertEquals(expResult, result);
 
     }
-@Test
+
+    @Test
     public void testCalculateBMUpperBoundary() {
         System.out.println("calculateBM");
         double in1 = 1.3;
@@ -122,7 +124,8 @@ public class ScubaCalculationsTest {
         String result = ScubaCalculations.calculatePP(in1, in2);
         assertEquals(expResult, result);
     }
-@Test
+
+    @Test
     public void testCalculatePPLowerBoundary() {
         System.out.println("calculatePP");
         double in1 = 0.22;
@@ -131,7 +134,8 @@ public class ScubaCalculationsTest {
         String result = ScubaCalculations.calculatePP(in1, in2);
         assertEquals(expResult, result);
     }
-@Test
+
+    @Test
     public void testCalculatePPUpperBoundary() {
         System.out.println("calculatePP");
         double in1 = 0.5;
@@ -168,7 +172,8 @@ public class ScubaCalculationsTest {
         assertEquals(expResult, result);
 
     }
-@Test
+
+    @Test
     public void testCalculateEADLowerBoundary() {
         System.out.println("calculateEAD");
         double in1 = 0.22;
@@ -178,7 +183,8 @@ public class ScubaCalculationsTest {
 
         assertEquals(expResult, result);
     }
-@Test
+
+    @Test
     public void testCalculateEADUpperBoundary() {
         System.out.println("calculateEAD");
         double in1 = 0.5;
