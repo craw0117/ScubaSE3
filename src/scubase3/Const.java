11d2d70f0d29cea394ba686aecafb8fc24f00272
simcr <simcr@10.14.2.142>
2018-04-23 13:52:49 +0930

    Added SMOD PP constant

--- src/scubase3/Const.java
+++ src/scubase3/Const.java
@@ -2,14 +2,20 @@ package scubase3;
 
 /**
  * Contains constant variables used throughout the application
- * 
+ *
  * @author liu1028, eden0021, mitc0341, craw0117, kris0068
  */
 public class Const {
 
-    // bitmap flags  
+    // bitmap flags
     public static final int FLAG_O2_PRESSURE = 1;
     public static final int FLAG_FRAC_OXYGEN = 2;
     public static final int FLAG_DEPTH = 4;
+
+    /**
+     * The partial pressure constant for the Standard Maximum Operating Depth
+     * calculation.
+     */
+    public static final double SMOD_PP_VALUE = 1.4;
 
 }
