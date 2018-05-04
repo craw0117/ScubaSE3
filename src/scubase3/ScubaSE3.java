49ffa2c685c38361994ff73a5386213bf4052845
nathan <iseestupidpeople70@gmail.com>
2018-05-04 09:40:07 +0930

    Changed look and feel to system look and feel

--- src/scubase3/ScubaSE3.java
+++ src/scubase3/ScubaSE3.java
@@ -1,5 +1,7 @@
 package scubase3;
 
+import javax.swing.UIManager;
+
 /**
  * Main class for ScubaSE3 project, holds the main execution method
  *
@@ -19,18 +21,10 @@ public class ScubaSE3 {
     }
 
     public static void main(String[] args) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(ScubaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
@@ -40,7 +34,6 @@ public class ScubaSE3 {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ScubaFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
